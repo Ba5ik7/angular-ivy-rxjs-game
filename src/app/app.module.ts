c449982a3c3e516589235849018d34d1e9ab449c
Ba5ik7 <dusellfam@gmail.com>
2020-07-31 18:55:19 -0400

    Create basic components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { AppService } from './shared/services/app.service';
+import { SidePanelService } from './shared/services/side-panel.service';
+import { ModalService } from './shared/services/modal.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeaderComponent, FooterComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [AppService, SidePanelService, ModalService]
 })
 export class AppModule { }
