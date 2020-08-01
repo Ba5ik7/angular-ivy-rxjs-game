d1af2f03305a128f3412c6fc7e2b631d3d0d7fff
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 00:03:04 -0400

    4 files modified; Add flex module and setup page layout grid.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -22,6 +23,7 @@ import { AppRoutingModule } from './app-routing.module';
     MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
   ],
   declarations: [ AppComponent, HeaderComponent, FooterComponent, SidenavButtonListComponent ],
   bootstrap:    [ AppComponent ],
