ce9207ed6ea2621ecb28776c48fe69942334f52f
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 01:52:31 -0400

    1 file modified, 2 added; Added progression component in Dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { StatsComponent } from './components/stats/stats.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { ProgressionComponent } from './components/progression/progression.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
-  declarations: [DashboardComponent, StatsComponent, LeaderboardComponent]
+  declarations: [DashboardComponent, StatsComponent, LeaderboardComponent, ProgressionComponent]
 })
 export class DashboardModule {}
