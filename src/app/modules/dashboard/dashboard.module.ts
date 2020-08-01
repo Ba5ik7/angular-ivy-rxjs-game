5ac778e2dcdcaf50f8b8e8f797ee3b241c41d755
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 01:41:58 -0400

    2 files modified, 2 added; Added leaderboard component to the dashboard module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { StatsComponent } from './stats/stats.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
-  declarations: [DashboardComponent, StatsComponent]
+  declarations: [DashboardComponent, StatsComponent, LeaderboardComponent]
 })
 export class DashboardModule {}
