e912bd15b3f7edd82c0dd50e9d841ad413c90ae1
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 01:54:29 -0400

    1 file modified, 2 added; Added MotdComponent to DashboardModule

---
+++ src/app/modules/dashboard/components/motd/motd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motd',
+  templateUrl: './motd.component.html',
+  styleUrls: ['./motd.component.css']
+})
+export class MotdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { StatsComponent } from './components/stats/stats.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 import { ProgressionComponent } from './components/progression/progression.component';
+import { MotdComponent } from './components/motd/motd.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
-  declarations: [DashboardComponent, StatsComponent, LeaderboardComponent, ProgressionComponent]
+  declarations: [DashboardComponent, StatsComponent, LeaderboardComponent, ProgressionComponent, MotdComponent]
 })
 export class DashboardModule {}
