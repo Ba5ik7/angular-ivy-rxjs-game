e7aef7720ad49fb5ca9ac22537636c2070916051
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 01:35:32 -0400

    1 file modified, 2 added; Add stats component to the dashboard module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, StatsComponent]
 })
 export class DashboardModule {}
