def32d88d4f16af17fcb783b59c21894d9678097
Ba5ik7 <dusellfam@gmail.com>
2020-08-01 01:47:04 -0400

    1 file modified, 6 added, 4 deleted

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
-import { StatsComponent } from './stats/stats.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
